bd54533250f5615945c4a4f790d7cc7a578a7b26
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-09-09 19:45:00 -0300

    Validação de campos no controller e exibição de erros nas telas. Criação de um componente de para exibir os erros

--- resources/views/courses/coursesBtn.blade.php
+++ resources/views/courses/coursesBtn.blade.php
@@ -1,2 +1,2 @@
 <a class="btn btn-outline-info" href="/cursos/{{$course->id}}/edit">Alterar</a>
-<a class="btn btn-outline-danger" href="/cursos/{{$course->id}}/delete">Excluir</a>
+<a class="btn btn-outline-danger" href="/cursos/{{$course->id}}/delete"><svg-icon><src href="sprite.svg#si-glyph-trash" /></svg-icon>Excluir</a>
