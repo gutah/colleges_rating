f4402a9d8f947529656f1ca4f405bfbb249e09f3
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-09-11 19:04:51 -0300

    Exibindo tipo da universidade

--- resources/views/colleges/show.blade.php
+++ resources/views/colleges/show.blade.php
@@ -4,11 +4,25 @@
 
     <h1 class="starter-template">{{$college->name}}</h1>
 
-    <div class="col col-12 text-justify">
-      <p>{{$college->description}}</p>
+    <div class="row">
+      <div class="jumbotron col col-8 text-justify">
+        <h2>Sobre</h2>
+        <hr>
+        <p>{{$college->description}}</p>
+      </div>
+      <div class="col col-4">
+        <h4>Tipo</h4>
+        @if ($college->type == 0)
+          <p>Pública</p>
+        @endif
+        @if ($college->type <> 0)
+          <p>Privada</p>
+        @endif
+      </div>
     </div>
+
     <div class="col col-12 text-right">
-      <hr>
+      <hr class="my-4">
         @include('colleges.collegesBtn')
     </div>
 
