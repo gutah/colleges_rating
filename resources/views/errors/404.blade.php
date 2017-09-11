e7c22e7c1c0cc55264b4b55221d0dbaad16a61a5
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-09-10 23:21:52 -0300

    Criando página de erro 404.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('content')
+
+  <div class="jumbotron">
+    <div class="container text-center">
+      <h1 class="display-3">Whoops!</h1><h2>Paginá não encontrada =s</h2>
+      <p class="lead">Por favor entre em contato caso ache que algo deveria existir aqui.</p>
+      <hr class="my-4">
+      <a class="btn btn-outline-primary btn-lg" href="/" role="button">Me leve para casa</a>
+      <a class="btn btn-outline-dark btn-lg" href="#" role="button">Entre em contato</a>
+
+    </div>
+  </div>
+
+@endsection
