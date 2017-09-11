1bf90777329d4d9e2a5eafadcb1761755d51ff10
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-09-11 18:59:48 -0300

    Bug Tentativa de correção de Bug propper.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,12 +10,18 @@
 
     <title>Colleges Rank</title>
 
+    <!-- THIS LINE -->
+    {{--<script src="/js/popper.js"></script>--}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/popper.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    {{--<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>--}}
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    <!-- THIS LINE -->
-   <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-   <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
 
     <!-- Custom styles for this template -->
     <link href="/css/starter-template.css" rel="stylesheet">
@@ -30,7 +36,6 @@
         @yield('content')
 
     </div><!-- /.container -->
-    <script src="/js/popper.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+
   </body>
 </html>
