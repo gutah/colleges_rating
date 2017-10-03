9534925b1d4b7fa806137bd4b6101eb574678af2
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-10-03 20:15:21 -0300

    Corrigindo bug de integração git

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,17 +12,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Colleges Rank</title>
-    
-<<<<<<< HEAD
+
     <script type="text/javascript" src="{{asset('js/app.js')}}">
 
     </script>
     <!-- THIS LINE -->
-    {{--<script src="/js/popper.js"></script>--}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/popper.min.js"></script>
-=======
-    <!-- Scripts -->
->>>>>>> desenv
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 
