8be1b1f2576b2ebcc753397e8579338f0899c31b
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-10-07 12:05:02 -0300

    Criando seeder para usuário

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         //$this->call(UsersTableSeeder::class);
         $this->call(CourseTableSeeder::class);
         $this->call(CollegeTableSeeder::class);
+        $this->call(UserTableSeeder::class);
 
     }
 }
