cbf78db9160be4f220369c0f744b7b34984b58ce
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-10-07 12:05:39 -0300

    User seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'g-maciel@outlook.com',
+            'password' => bcrypt('secret'),
+      ]);
+
+      factory(App\College::class, 5)->create();
+    }
+}
