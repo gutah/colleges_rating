6cfc9850c22dcb99665056304aa887f5cfc49a86
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-10-13 22:01:56 -0300

    PAdronizando exibição de cursos e universidades

--- resources/views/colleges/index.blade.php
+++ resources/views/colleges/index.blade.php
@@ -1,67 +1,16 @@
 @extends('layouts.master')
 
 @section('content')
-
-  <div class="starter-template">
-    <h1>Universidades</h1>
-  </div>
-  <div class="card-deck">
-    <div class="row">
-      @foreach ($colleges as $college)
-        <div class="col-lg-4">
-        <div class="card text-white bg-dark mb-3" style="max-width: 20rem;">
-
-          <div class="card-header">
-            <h4 class="card-title">{{$college->name}}</h4>
-          </div>
-
-          <div class="card-body">
-            {{--<h4 class="card-title"><a href="/cursos/{{$college->id}}">{{$college->name}}</a></h4>--}}
-            <p class="card-text">{{substr($college->description, 0, 95)}}</p>
-            <p class="card-text"><small class="text-muted">Ultima atualização {{ $college->updated_at->diffForHumans(null, true) }}</small></p>
-            <div class="card-footer bg-dark border-dark text-right text-center">
-              {{--@include('courses.coursesBtn')--}}
-
-              <a href="/universidades/{{$college->slug}}" class="btn btn-outline-info btn-lg btn-block">Ver mais</a>
-            </div>
-
-          </div>
-        </div>
-      </div>
-      @endforeach
-    </div>
+  <div class="container">
+    <div class="starter-template">
+      <h1>Universidades</h1>
   </div>
 
-{{--  <nav aria-label="Page navigation example">
-    <ul class="pagination">
-      <li class="page-item">
-        <a class="page-link" href="{{$colleges->previousPageUrl()}}" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-          <span class="sr-only">Previous</span>
-        </a>
-      </li>
-
-
-          <li class="page-item"><a class="page-link" href="{{$colleges->links()}}">1</a></li>
-
-
-
-      <li class="page-item">
-        <a class="page-link" href="{{$colleges->nextPageUrl()}}" aria-label="Next">
-          <span aria-hidden="true">&raquo;</span>
-          <span class="sr-only">Next</span>
-        </a>
-      </li>
-    </ul>
-  </nav>--}}
-  {{ $colleges->links('vendor.pagination.default') }}
-
+  @include('layouts.cards')
 
   <hr>
   <div class="col col-12">
-
     <a href="/universidades/create" class="btn btn-outline-primary btn-lg float-right" role="button" aria-pressed="true">Adicionar</a>
-
   </div>
 
 @endsection
--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,42 +1,17 @@
 @extends('layouts.master')
 
 @section('content')
-
-  <div class="starter-template">
-    <h1>Cursos</h1>
-  </div>
-  <div class="card-deck">
-    <div class="row">
-      @foreach ($courses as $course)
-        <div class="col-lg-4">
-        <div class="card text-white bg-dark mb-3" style="max-width: 20rem;">
-
-          <div class="card-header">
-            <h4 class="card-title">{{$course->name}}</h4>
-          </div>
-
-          <div class="card-body">
-            {{--<h4 class="card-title"><a href="/cursos/{{$course->id}}">{{$course->name}}</a></h4>--}}
-            <p class="card-text">{{substr($course->description, 0, 95)}}</p>
-            <p class="card-text"><small class="text-muted">Ultima atualização {{ $course->updated_at->diffForHumans(null, true) }}</small></p>
-            <div class="card-footer bg-dark border-dark text-right text-center">
-              {{--@include('courses.coursesBtn')--}}
-
-              <a href="/cursos/{{$course->slug}}" class="btn btn-outline-info btn-lg btn-block">Ver mais</a>
-            </div>
-
-          </div>
-        </div>
-      </div>
-      @endforeach
+  <div class="containar">
+    <div class="starter-template">
+      <h1>Cursos</h1>
     </div>
+
+    @include('layouts.cards')
+
+    <hr>
+    <div class="col col-12">
+      <a href="/cursos/create" class="btn btn-outline-primary btn-lg float-right" role="button" aria-pressed="true">Adicionar</a>
+    </div>
+
   </div>
-  {{ $courses->links('vendor.pagination.default') }}
-  <hr>
-  <div class="col col-12">
-
-    <a href="/cursos/create" class="btn btn-outline-primary btn-lg float-right" role="button" aria-pressed="true">Adicionar</a>
-
-  </div>
-
 @endsection
