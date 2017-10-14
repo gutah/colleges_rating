4ad56ba71e3cbce814aec96246d2a9f90b23830f
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-10-13 21:53:20 -0300

    Dando nome as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
 Route::get('/', function () {
   return view('welcome');
 
-});
+})->name('home');
 
 //Modulo de Cursos
-Route::get('/cursos', 'CoursesController@index');
+Route::get('/cursos', 'CoursesController@index')->name('cursos');
 Route::post('/cursos', 'CoursesController@store');
 Route::get('/cursos/create', 'CoursesController@create');
 Route::get('/cursos/{course}', 'CoursesController@show');
@@ -16,7 +20,7 @@ Route::delete('/cursos/{courses}', 'CoursesController@destroy');
 Route::get('/cursos/{course}/edit', 'CoursesController@edit');
 
 //Modulo de Universidades
-Route::get('/universidades', 'CollegesController@index');
+Route::get('/universidades', 'CollegesController@index')->name('universidades');
 Route::post('/universidades', 'CollegesController@store');
 Route::get('/universidades/create', 'CollegesController@create');
 Route::get('/universidades/{college}', 'CollegesController@show');
@@ -30,7 +34,6 @@ Route::get('/users', function() {return "Página de Usuários";});
 
 
 /*
-
 GET /posts
 GET /posts/create
 POST /posts
@@ -38,9 +41,4 @@ GET /posts/{id}/edit
 GET /posts/{id}/
 PATCH /posts/{id}
 DELETE /posts/{id}
-
 */
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
